936fd84aee801ce2819ce8c35c7d2ec43c03dfb0
shin-min-hyeok <smw262@naver.com>
2023-05-25 18:08:31 +0900

    WebClass

--- @@REDEIGN@@/js/Vons.js
+++ @@REDEIGN@@/js/Vons.js
@@ -1,8 +1,11 @@
 window.onload = function () {
-  let pagiNation = document.querySelectorAll(".pagiNation>li")
-  let  train = document.querySelector(".MainSlide")
-  let trainList = train.children;
-  let count = 0;
+  
+  let MainBanner = new Swiper(".MainBanner",{
+    loop:true, 
+
+
+
+  })
 
   
 
