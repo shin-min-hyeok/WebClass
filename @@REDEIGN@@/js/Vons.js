e01e41df3f3e989de87923456ab07d7d07e1c747
shin-min-hyeok <smw262@naver.com>
2023-05-26 18:08:26 +0900

    WebClass

--- @@REDEIGN@@/js/Vons.js
+++ @@REDEIGN@@/js/Vons.js
@@ -1,7 +1,10 @@
 window.onload = function () {
   
   let MainBanner = new Swiper(".MainBanner",{
-    loop:true, 
+    loop:true,               // 반복
+    direction:"horizontal",  // 세로방향  가로(horizontal)
+    slidesPerView:1, 
+    autoplay:true  
 
 
 
---
+++ pearlabyss-ex/js/common.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+
+  let newsSlider = new Swiper(".news_station",{
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints:{
+      280:{
+        slidesPerView : 1,
+      },
+      768:{
+        slidesPerView : 3,
+      }
+    }
+  });
+
+}
