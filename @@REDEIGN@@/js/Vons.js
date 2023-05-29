ae111e38f21990699bca3ffe3735ac2c0b368e9a
신민혁 <smw262@naver.com>
2023-05-29 23:52:00 +0900

    WebClass

--- @@REDEIGN@@/js/Vons.js
+++ @@REDEIGN@@/js/Vons.js
@@ -1,19 +1,24 @@
 window.onload = function () {
-  
-  let MainBanner = new Swiper(".MainBanner",{
-    loop:true,               // 반복
-    direction:"horizontal",  // 세로방향  가로(horizontal)
-    slidesPerView:1, 
-    autoplay:true  
-
-
-
-  })
-
-  
-
-
-
-
-
-}
+  let MainBanner = new Swiper(".MainBanner", {
+    loop: true, // 반복
+    direction: "horizontal", // 세로방향  가로(horizontal)
+    slidesPerView: 1,
+    autoplay: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets", // 모양
+      clickable: true, // 클릭시 클릭페이지로 이동
+    },
+    autoplay: {
+      delay: 2300,
+    },
+    speed: 1000,
+  });
+  let MenuSlide = new Swiper(".MenuSlide", {
+    loop: true, // 반복
+    direction: "horizontal", // 세로방향  가로(horizontal)
+    spaceBetween: 30,
+    slidesPerView: 3,
+    autoplay: true,
+  });
+};
