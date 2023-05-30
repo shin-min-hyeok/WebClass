db45b723bfc9c68e77e9c383c96a7a875ce4d003
shin-min-hyeok <smw262@naver.com>
2023-05-30 18:10:44 +0900

    WebClass

--- pearlabyss-ex/js/common.js
+++ pearlabyss-ex/js/common.js
@@ -13,5 +13,33 @@ window.onload = function(){
       }
     }
   });
+  let noticeSlieder = new Swiper(".notice",{
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints:{
+      280:{
+        slidesPerView : 1,
+      },
+      768:{
+        slidesPerView : 4,
+      }
+    }
+  });
+}
 
+
+//객체-> 어떤사물을 표현하기 위해
+let person1 = {
+  name : "정현진",
+  age : 38,
+  score : {
+    ko: 100,
+    en: 90,
+    math : 80,
+  },
+  friends : {
+    oldfriends : ['상수','시홍','현종'],
+    scgoolfriends :['민수' ,'영훈','준우']
+  }
 }
