9bee716fec626ec096f034cd82d525e03468e108
shin-min-hyeok <smw262@naver.com>
2023-06-20 17:14:02 +0900

    WebClass

--- 40.effect/js/study3.js
+++ 40.effect/js/study3.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   
 
-  $(".btn").click(function(){
-    // textAni(".title",{
-    //   aniDelay:0.5,
-    //   aniSpeed:1,
-    //   textSize:80
-    // })
+  
+  textAni(".title",{
+    textSize:100,
+    aniSpeed:0.2,
+    effect:"moveTopToBottom"
   })
-  textAni(".title",{})
-
+  textAni(".title2",{
+    textSize:50,
+    aniSpeed:0.5,
+    aniDelay:0.01,
+    effect:"moveTopToBottom"
+  })
 })
