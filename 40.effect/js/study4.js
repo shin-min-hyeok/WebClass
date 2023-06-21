4f3239bdc7ce04a0bb416fcf8c038a03c5d8668e
shin-min-hyeok <smw262@naver.com>
2023-06-21 18:18:00 +0900

    WebClass

--- 40.effect/js/study4.js
+++ 40.effect/js/study4.js
@@ -10,5 +10,55 @@ $(document).ready(function(){
     </li>
     `)
   }
+  $(".stationMenu>li").click(function(){
+    let idx = $(this).index() // 클릭한 리스트의 순번을 리턴함.
+    count = idx*5; //정해진 순번에 5를 곱한 값을 전역변수 count에 저장함으로써 휠을 내렸을 때 이동되는 기능이 정상적으로 작동되게끔 해주는 코드
+    // 06 ->  0px
+    // 06 ->  -1000px
+    // 11 ->  -2000px
+    // 16 ->  -3000px
+    $(".train").css("transform","translateX("+(-1000*idx)+"px)")
+  })
+  
+
+  let count = 0;
+  $(".station").on("wheel DOMMouseScroll", function (event) {
+    // console.log(event)
+    let E = event.originalEvent;
+    let delta = 0;
+    if (E.detail) {
+      delta = E.detail * -40;
+    } else {
+      delta = E.wheelDelta;
+    }
+
+    if (delta < 0) {
+      //마우스 휠을 내렸을 때
+      count++;
+      if(count>15){count =15}
+      $(".train").css("transform","translateX("+(-200*count)+"px)")
+    } else {
+      //마우스 휠을 올렸을 때
+      count--
+      if(count <0){count =0}
+      $(".train").css("transform","translateX("+(-200*count)+"px)")
+    }
+    return false;
+  });
+
+  // 클릭한 리스트(기차칸)의 활성화 스타일 기능
+  $(".train>li").click(function(){
+    $(".train>li").removeClass("on")
+    $(this).addClass("on")
+    let idx = $(this).index()
+    count = idx; //동기화시킴
+    $(".train").css("transform","translateX("+(-200*idx)+"px)")
+  })
+
+  // 닫는버튼 기능
+  $(".btnClose").click(function(){
+    $(".train>li").removeClass("on")
+    return false; // 상위태그로 클릭이벤트가 전달되지않게(버블링되지않게) 설정
+  })
 })
 
